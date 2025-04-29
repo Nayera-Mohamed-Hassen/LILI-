3c2b3e171a8e0b6640dbe8ba48322363f94875f9
Nayera-Mohamed-Hassen <mohamednayera437@gmail.com>
2025-04-29 17:55:40 +0300

    init setup

---
+++ lib/needed.dart
@@ -0,0 +1,18 @@
+//button
+// ElevatedButton(
+// onPressed: () {},
+// style: ElevatedButton.styleFrom(
+// backgroundColor: Color(0xFF3E5879),
+//
+// // const Color(0xFF3E5879)
+// minimumSize: Size(315, 55),
+// shape: RoundedRectangleBorder(
+// side: BorderSide(width: 1),
+// borderRadius: BorderRadius.circular(5),
+// ),
+// ),
+// child: Text(
+// 'Log In',
+// style: TextStyle(color: Colors.white, fontSize: 18),
+// ),
+// ),
---
+++ lib/pages/init_setup_page.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+
+class InitSetupPage extends StatelessWidget {
+  const InitSetupPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFF5EFE7),
+      body: Padding(
+        padding: const EdgeInsets.all(24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: 400,
+              height: 200,
+              clipBehavior: Clip.antiAlias,
+              decoration: BoxDecoration(color: const Color(0xFFF5EFE7)),
+              child: Stack(
+                children: [
+                  Positioned(
+                    left: 20,
+                    top: 100,
+                    child: Text(
+                      'Set Up',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: const Color(0xFF213555),
+                        fontSize: 64,
+                        fontFamily: 'Roboto',
+                        fontWeight: FontWeight.w400,
+                        height: 1.20,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            DropdownButtonFormField<String>(
+              decoration: const InputDecoration(
+                labelText: 'Select role',
+                prefixIcon: Icon(Icons.person_outline),
+              ),
+              items: const [
+                DropdownMenuItem(value: 'Admin', child: Text('Admin')),
+                DropdownMenuItem(value: 'Member', child: Text('Member')),
+              ],
+              onChanged: (value) {},
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Color(0xFF3E5879),
+
+                // const Color(0xFF3E5879)
+                minimumSize: Size(315, 55),
+                shape: RoundedRectangleBorder(
+                  side: BorderSide(width: 1),
+                  borderRadius: BorderRadius.circular(5),
+                ),
+              ),
+              child: Text(
+                'Add User',
+                style: TextStyle(color: Colors.white, fontSize: 18),
+              ),
+            ),
+            const SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Color(0xFF3E5879),
+
+                // const Color(0xFF3E5879)
+                minimumSize: Size(315, 55),
+                shape: RoundedRectangleBorder(
+                  side: BorderSide(width: 1),
+                  borderRadius: BorderRadius.circular(5),
+                ),
+              ),
+              child: Text(
+                'Confirm',
+                style: TextStyle(color: Colors.white, fontSize: 18),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
