321d0a87de5c68b1afd06c0c45c442d9f4a89c87
Nayera-Mohamed-Hassen <mohamednayera437@gmail.com>
2025-04-29 17:36:21 +0300

    hosting page

---
+++ lib/pages/hosting_page.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class HostingPage extends StatelessWidget {
+  const HostingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFF5EFE7),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Color(0xFF3E5879),
+
+                // const Color(0xFF3E5879)
+                minimumSize: Size(315, 55),
+                shape: RoundedRectangleBorder(
+                  side: BorderSide(width: 1),
+                  borderRadius: BorderRadius.circular(5),
+                ),
+              ),
+              child: Text(
+                'Host House',
+                style: TextStyle(color: Colors.white, fontSize: 18),
+              ),
+            ),
+            const SizedBox(height: 30),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Color(0xFF3E5879),
+
+                // const Color(0xFF3E5879)
+                minimumSize: Size(315, 55),
+                shape: RoundedRectangleBorder(
+                  side: BorderSide(width: 1),
+                  borderRadius: BorderRadius.circular(5),
+                ),
+              ),
+              child: Text(
+                'Join House',
+                style: TextStyle(color: Colors.white, fontSize: 18),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
