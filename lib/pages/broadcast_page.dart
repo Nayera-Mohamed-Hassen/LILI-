08a8164e51b726a34b835f2485f9c58b63d3a693
Nayera-Mohamed-Hassen <mohamednayera437@gmail.com>
2025-04-29 15:04:25 +0300

    broadcast page and signing

---
+++ lib/pages/broadcast_page.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+class BroadcastPage extends StatelessWidget {
+  const BroadcastPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFF5EFE7), // same beige
+      // const Color(0xFFF5EFE7)
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 32.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/images/LILI_logo.png',
+                height: 261,
+                width: 264,
+              ),
+              // SizedBox(height: 30),
+              Text(
+                'LILI',
+                style: TextStyle(
+                  fontSize: 36,
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xFF213555),
+                ),
+              ),
+              SizedBox(height: 16),
+              Text(
+                'A smart intelligent platform for home setup management',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 16, color: Color(0xFF213555)),
+              ),
+              SizedBox(height: 50),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/signing');
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color(0xFF3E5879),
+
+                  // const Color(0xFF3E5879)
+                  minimumSize: Size(315, 55),
+                  shape: RoundedRectangleBorder(
+                    side: BorderSide(width: 1),
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                ),
+                child: Text(
+                  'Get Started',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/signing_page.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+
+class SigningPage extends StatelessWidget {
+  const SigningPage({super.key});
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: const Color.fromARGB(255, 18, 32, 47),
+      ),
+      home: Scaffold(body: ListView(children: [Signing()])),
+    );
+  }
+}
+
+class Signing extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          width: 390,
+          height: 824,
+          clipBehavior: Clip.antiAlias,
+          decoration: BoxDecoration(color: const Color(0xFFF5EFE7)),
+          child: Stack(
+            children: [
+              Positioned(
+                left: 20,
+                top: 198,
+                child: Text(
+                  'LILI',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: const Color(0xFF213555),
+                    fontSize: 64,
+                    fontFamily: 'Roboto',
+                    fontWeight: FontWeight.w400,
+                    height: 1.20,
+                  ),
+                ),
+              ),
+              Positioned(
+                left: 38,
+                top: 357,
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  spacing: 8,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/signing');
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Color(0xFF3E5879),
+
+                        // const Color(0xFF3E5879)
+                        minimumSize: Size(315, 55),
+                        shape: RoundedRectangleBorder(
+                          side: BorderSide(width: 1),
+                          borderRadius: BorderRadius.circular(5),
+                        ),
+                      ),
+                      child: Text(
+                        'Sign Up',
+                        style: TextStyle(color: Colors.white, fontSize: 18),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Positioned(
+                left: 38,
+                top: 435,
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  spacing: 8,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/signing');
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Color(0xFF3E5879),
+
+                        // const Color(0xFF3E5879)
+                        minimumSize: Size(315, 55),
+                        shape: RoundedRectangleBorder(
+                          side: BorderSide(width: 1),
+                          borderRadius: BorderRadius.circular(5),
+                        ),
+                      ),
+                      child: Text(
+                        'Log in',
+                        style: TextStyle(color: Colors.white, fontSize: 18),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
