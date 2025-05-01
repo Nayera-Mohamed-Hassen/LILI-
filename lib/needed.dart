6928f360cf7ccce84b7f85c4f50315349a184da8
Nayera-Mohamed-Hassen <mohamednayera437@gmail.com>
2025-05-01 09:11:22 +0300

    alot

--- lib/needed.dart
+++ lib/needed.dart
@@ -49,3 +49,17 @@
 // ],
 // ),
 // ),
+// Container(
+// margin: const EdgeInsets.symmetric(vertical: 20),
+// height: 200,
+// child: ListView(
+// // This next line does the trick.
+// scrollDirection: Axis.horizontal,
+// children: <Widget>[
+// Container(width: 160, color: Colors.red),
+// Container(width: 160, color: Colors.blue),
+// Container(width: 160, color: Colors.green),
+// Container(width: 160, color: Colors.yellow),
+// Container(width: 160, color: Colors.orange),
+// ],
+// ),
