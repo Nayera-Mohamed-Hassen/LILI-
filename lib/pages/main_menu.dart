bd057149ffdd498816e4252dac4c052cbd3aab74
ganna-salah <149871123+ganna-salah@users.noreply.github.com>
2025-05-02 23:28:43 +0300

    main menu added

--- lib/pages/main_menu.dart
+++ lib/pages/main_menu.dart
@@ -1,5 +1,18 @@
 import 'package:flutter/material.dart';
-import 'navbar.dart'; // Import the navbar
+import 'package:untitled4/pages/tasks_home_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MainMenuPage()
+    );
+  }
+}
 
 class MainMenuPage extends StatefulWidget {
   @override
@@ -7,40 +20,95 @@ class MainMenuPage extends StatefulWidget {
 }
 
 class _MainMenuPageState extends State<MainMenuPage> {
-  int _currentIndex = 1; // Default to MainMenuPage when selected
-
-  // Pages to display based on navigation bar selection
-  final List<Widget> _pages = [
-    Center(child: Text('Home Page Content')),
-    Center(child: Text('Main Menu Content')),
-    Center(child: Text('Emergency Content')),
-    Center(child: Text('Profile Content')),
-  ];
-
-  // Function to handle tab navigation
-  void _onTabTapped(int index) {
-    setState(() {
-      _currentIndex = index; // Update the current index based on tab selection
-    });
-
-    if (index == 0) {
-      Navigator.pushNamed(context, '/homepage'); // Navigate to HomePage
-    } else if (index == 1) {
-      Navigator.pushNamed(context, '/mainmenu'); // Stay on MainMenuPage
-    } else if (index == 2) {
-      Navigator.pushNamed(context, '/emergency'); // Navigate to EmergencyPage
-    } else if (index == 3) {
-      Navigator.pushNamed(context, '/profile'); // Navigate to ProfilePage
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xFFF5EFE7), // Beige background
+      body: Column(
+        children: [
+          SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Icon(Icons.notifications, color: Color(0xFF3E5879), size: 50),
+            ],
+          ),
+          SizedBox(height: 20),
+          TextField(
+            decoration: InputDecoration(
+              hintText: 'Search features...',
+              prefixIcon: Icon(Icons.search),
+              suffixIcon: Icon(Icons.filter_list),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(28),
+              ),
+            ),
+          ),
+          SizedBox(height: 20),
+          Expanded(child:
+          SingleChildScrollView(child:Column(
+            children: [
+              Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                buildButton("View Tasks", '/task home'),
+                buildButton("Recommend Recipe", '/recommendRecipe'),
+              ],
+            ),
+              SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  buildButton("Show Inventory", '/'),
+                  buildButton("Show Expenses", '/'),
+                ],
+              ),
+              SizedBox(height: 20),
 
-      body: _pages[_currentIndex], // Show corresponding content based on selected tab
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  buildButton("Add To Shopping List", '/'),
+                  buildButton("Open Calendar", '/'),
+                ],
+              ),
+              SizedBox(height: 20),
 
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  buildButton("Show Workout Plan", '/'),
+                  buildButton("Show Family Map", '/'),
+                ],
+              ),],
+          ) ,
+          ),
+          ),
+          SizedBox(height: 30),
+        ],
+      ),
+    );
+  }
+
+  Widget buildButton(String text, String route) {
+    return SizedBox(
+      width: 140,
+      height: 150,
+      child: ElevatedButton(
+        onPressed: () {
+          Navigator.pushNamed(context, route); // Navigate to respective screen
+        },
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Color(0xFF1F3354),
+          foregroundColor: Colors.white,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
+          ),
+        ),
+        child: Text(text,style: TextStyle(fontWeight: FontWeight.bold,) ,textAlign: TextAlign.center,)
+       ,),
     );
   }
 }
+
+
