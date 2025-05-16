0e8d9e4b1597864937a7679a1f54876522b55f3c
ganna-salah <149871123+ganna-salah@users.noreply.github.com>
2025-05-16 08:01:28 +0300

    main menu edited

--- lib/pages/main_menu.dart
+++ lib/pages/main_menu.dart
@@ -1,6 +1,7 @@
 import 'package:LILI/main.dart';
 import 'package:flutter/material.dart';
-import 'package:LILI/pages/tasks_home_page.dart';
+import 'package:gif_view/gif_view.dart';
+import 'package:gif/gif.dart';
 
 class MyApp extends StatelessWidget {
   @override
@@ -19,90 +20,102 @@ class _MainMenuPageState extends State<MainMenuPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       // Beige background
-      body: Stack(
-        children: [
-          Positioned.fill(
-            child: Image.asset(
-              'assets/backgrounds/homepage.png',
-              fit: BoxFit.cover,
+      body: Center(
+        child: Stack(
+          children: [
+            /********** this menu item is not needed but don't remove it ***********/
+            /********** as it affects the GIFs ***********/
+            Menuitem(
+              onPressed: () {
+                Navigator.pushNamed(context, '/task home');
+              },
+              text: "View Tasks",
+              image: "assets/images/tasks.gif",
             ),
-          ),
-          Column(
-            children: [
-              SizedBox(height: 20),
-              SizedBox(
-                width: MediaQuery.of(context).size.width - 30,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  // Aligns the icon to the right
-                  children: [
-                    Icon(
-                      Icons.notifications,
-                      color: Color(0xFFF2F2F2),
-                      size: 50,
-                    ),
-                  ],
-                ),
+
+            /********** this is the end of the none needed item *********/
+            Positioned.fill(
+              child: Image.asset(
+                'assets/backgrounds/homepage.png',
+                alignment: Alignment.center,
+                fit: BoxFit.cover,
               ),
-              SizedBox(height: 20),
-              SizedBox(
-                width: MediaQuery.of(context).size.width - 30,
-                child: TextField(
-                  decoration: InputDecoration(
-                    hintText: 'Search features...',
-                    prefixIcon: Icon(Icons.search),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(28),
-                    ),
-                  ),
-                ),
-              ),
-              SizedBox(height: 20),
-              Expanded(
-                child: SingleChildScrollView(
-                  child: Column(
+            ),
+            Column(
+              children: [
+                SizedBox(height: 20, width: MediaQuery.of(context).size.width),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width - 30,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    // Aligns the icon to the right
                     children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          buildButton("View Tasks", '/task home'),
-                          buildButton("Recommend Recipe", '/Recipe'),
-                        ],
-                      ),
-                      SizedBox(height: 20),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          buildButton("Show Inventory", '/inventory'),
-                          buildButton("Show Expenses", '/budget'),
-                        ],
-                      ),
-                      SizedBox(height: 20),
-
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          buildButton("Add To Shopping List", '/'),
-                          buildButton("Open Calendar", '/'),
-                        ],
-                      ),
-                      SizedBox(height: 20),
-
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          buildButton("Show Workout Plan", '/'),
-                          buildButton("Show Family Map", '/'),
-                        ],
+                      Icon(
+                        Icons.notifications,
+                        color: Color(0xFFF2F2F2),
+                        size: 50,
                       ),
                     ],
                   ),
                 ),
-              ),
-              SizedBox(height: 30),
-            ],
-          ),
-        ],
+                SizedBox(height: 20),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width - 30,
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Search features...',
+                      prefixIcon: Icon(Icons.search),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(28),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20),
+                Expanded(
+                  child: SingleChildScrollView(
+                    child: Column(
+                      children: [
+                        Menuitem(
+                          onPressed: () {
+                            Navigator.pushNamed(context, '/task home');
+                          },
+                          text: "View Tasks",
+                          image: "assets/images/tasks.gif",
+                        ),
+
+                        Menuitem(
+                          onPressed: () {
+                            Navigator.pushNamed(context, '/Recipe');
+                          },
+                          text: "Suggests Recipe",
+                          image: "assets/images/food.gif",
+                        ),
+
+                        Menuitem(
+                          onPressed: () {
+                            Navigator.pushNamed(context, '/inventory');
+                          },
+                          text: "Show Inventory",
+                          image: "assets/images/inventory.gif",
+                        ),
+
+                        Menuitem(
+                          onPressed: () {
+                            Navigator.pushNamed(context, '/budget');
+                          },
+                          text: "Track Expenses",
+                          image: "assets/images/money.gif",
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(height: 30),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -117,6 +130,132 @@ class _MainMenuPageState extends State<MainMenuPage> {
         },
         text: text,
       ),
+    );
+  }
+}
+
+class Menuitem extends StatelessWidget {
+  final VoidCallback onPressed;
+  final String text;
+  final String image;
+
+  const Menuitem({
+    required this.onPressed,
+    required this.text,
+    required this.image,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(height: 50),
+        GestureDetector(
+          onTap: this.onPressed,
+          child: Container(
+            width: 330,
+            height: 91,
+            decoration: ShapeDecoration(
+              color: const Color(0xFF3E5879),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(25),
+                side: BorderSide(color: Color(0xFF213555), width: 3),
+              ),
+            ),
+            child: Stack(
+              clipBehavior: Clip.none,
+              children: [
+                Positioned(
+                  left: 8,
+                  top: 36,
+                  child: SizedBox(
+                    width: 313,
+                    child: Text(
+                      this.text,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: const Color(0xFFF2F2F2),
+                        fontSize: 20,
+                        fontFamily: 'Inter',
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  left: 251,
+                  top: 46,
+                  child: Container(
+                    width: 70,
+                    height: 70,
+                    decoration: ShapeDecoration(
+                      color: const Color(0xFFF2F2F2),
+                      shape: RoundedRectangleBorder(
+                        side: BorderSide(
+                          width: 3,
+                          strokeAlign: BorderSide.strokeAlignCenter,
+                          color: const Color(0xFF213555),
+                        ),
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    ),
+                    child: const Center(
+                      child: Icon(
+                        //Icons.play_circle_outline,
+                        Icons.play_arrow_outlined,
+                        size: 60,
+                        color: Color(0xFF213555),
+                      ),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  left: 8,
+                  top: -19,
+                  child: Container(
+                    width: 69,
+                    height: 69,
+                    decoration: ShapeDecoration(
+                      color: const Color(0xFFF2F2F2),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    ),
+                    child: Stack(
+                      children: [
+                        Positioned(
+                          left: 0,
+                          top: 0,
+                          child: Container(
+                            child: GifView.asset(
+                              this.image,
+                              height: 200,
+                              width: 200,
+                              frameRate: 25,
+                            ),
+                            width: 70,
+                            height: 70,
+                            decoration: ShapeDecoration(
+                              shape: RoundedRectangleBorder(
+                                side: BorderSide(
+                                  width: 3,
+                                  color: const Color(0xFF213555),
+                                ),
+                                borderRadius: BorderRadius.circular(15),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
