4d046900c308a9327943a94aa027c248b0ac32a2
ganna-salah <149871123+ganna-salah@users.noreply.github.com>
2025-06-04 09:17:01 +0300

    remove config

--- lib/config/api_config.dart
+++
@@ -1,3 +0,0 @@
-class ApiConfig {
-  static const String baseUrl = 'http://localhost:8000';  // Update this with your FastAPI backend URL
-} 
--- lib/services/transaction_service.dart
+++ lib/services/transaction_service.dart
@@ -1,6 +1,5 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
-import '../config/api_config.dart';
 
 class TransactionService {
   static const String baseUrl = 'http://10.0.2.2:8000';
